0942f249d35fcd9ccd628cdfee26e193aa5d1a04
Rinor Sadiku <rinor.sadiku@ornio.no>
2020-09-30 00:27:07 +0200

    fixed ingress-nginx bug

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -4,7 +4,7 @@ export default ({ req }) => {
     if (typeof window === 'undefined') {
         // On the server
         return axios.create({
-            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'http://prod-app-ticketing.xyz',
             headers: req.headers
         });
     } else {
