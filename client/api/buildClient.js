b7cc176e4b74bc22062e1bfe31d4f39201df8cfc
Rinor Sadiku <rinor.sadiku@ornio.no>
2020-10-01 13:31:59 +0200

    added prod dockerfile for tickets

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -4,7 +4,7 @@ export default ({ req }) => {
     if (typeof window === 'undefined') {
         // On the server
         return axios.create({
-            baseURL: 'http://prod-app-ticketing.xyz',
+            baseURL: process.env.BASE_URL,
             headers: req.headers
         });
     } else {
